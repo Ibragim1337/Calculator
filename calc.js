4c46515dba24898504abeb1e12690014b4977eef
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2023-11-28 23:51:46 +0400

    complete main calc logic

--- calc.js
+++ calc.js
@@ -8,12 +8,75 @@ const action = ['-', '+', 'X', '/'];
 
 const out = document.querySelector(".pNumbers");
 
+
+//функция очистки на кнопку AC
 function clearAll(){
-  let a = '';
-  let b = '';
-  let sign  = '';
-  let finish = false; 
+   a = '';
+   b = '';
+   sign  = '';
+   finish = false; 
   out.textContent = 0;
 }
 
-document.querySelector('.ac'),onclick = clearAll;
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons-container').onclick = (event) => {
+  if(!event.target.classList.contains('btn')) return;
+  if(event.target.classList.contains('ac')) return;
+
+  out.textContent = '';
+  const key = event.target.textContent;
+
+
+  if (digit.includes(key)){
+    if(b === "" && sign === ""){
+      a += key;
+      console.log(a, b, sign);
+      out.textContent = a;
+    }
+    else if (a !== '' && b !== '' && finish){
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key;
+      out.textContent = b;
+    }
+
+    return;
+  }
+
+  if (action.includes(key)){
+    sign = key;
+    out.textContent = sign;
+  }
+
+  if (key === "=") {
+    if(b === '') b = a;
+    switch (sign) {
+      case "+":
+        a = (+a) + (+b);
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "X":
+        a = a * b;
+        break;
+      case "%":
+        a = a % b;
+        break;
+      case '/':
+        if (b === "0"){
+          out.textContent = "на ноль делишь?"
+          a = '';
+          b = '';
+          sign = '';
+          return
+        }
+        a = a / b;
+    }
+    finish = true;
+    out.textContent = a;
+  }
+}
