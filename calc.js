e30506daaa41658e7afdd407a2cfbdadafcae4fa
Alakparov Ibragim <Alakp_mr11.itstep.edu.az>
2023-11-28 21:35:51 +0400

    clear all method

---
+++ calc.js
@@ -0,0 +1,19 @@
+let a = '';
+let b = '';
+let sign  = '';
+let finish = false; 
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+const out = document.querySelector(".pNumbers");
+
+function clearAll(){
+  let a = '';
+  let b = '';
+  let sign  = '';
+  let finish = false; 
+  out.textContent = 0;
+}
+
+document.querySelector('.ac'),onclick = clearAll;
